1cecaa97ff5a9e10b0656d2c9392516c7df90c5d
Jordan Alba <jordan008@live.ca>
2021-08-31 11:34:16 -0400

    Add project files.

---
+++ HelloWorld_Android-IOS/HelloWorld_Android-IOS/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld_Android_IOS
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count=0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIncrease_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            lblValue.Text = count.ToString();
+        }
+
+        private void btnDecrease_Clicked(object sender, EventArgs e)
+        {
+            count--;
+            lblValue.Text = count.ToString();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            display.Text = string.Format("{0}'s Counter", e.NewTextValue);
+        }
+    }
+}
